2d90b1713d2a5c5e9f279c15b7bf49daca23c17f
Daya Avila :) <43186207+dayaavila@users.noreply.github.com>
2020-11-21 20:26:47 +0100

    1era prueba
    1era prueba

---
+++ FizzBuzzApi/FizzBuzzApi/Controllers/FizzBuzzController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+        public static string GetOutPut(int number)
+        {
+            string numberOutPut;
+
+            if ((number % 3 == 0) && (number % 5 == 0)) 
+            {//2145
+                numberOutPut = "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {//36
+                numberOutPut = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {//40
+                numberOutPut = "buzz";
+            }
+            else
+            {
+                numberOutPut = number.ToString();
+            }
+            return numberOutPut;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetFizzBuzz(int number)
+        {
+            var result = GetOutPut(number);
+            return result;
+        }
+    }
+}
